18a78785896bcf8414f0201a09ac73aaf7a27144
Ravikiran522 <ravikirangundala21@gmail.com>
2022-07-18 16:26:15 +0530

    Put and Delete in node api using mongoose

--- api/admin.js
+++ api/admin.js
@@ -25,13 +25,46 @@ router.post('/', async (req, res) => {
   };
   //const adminmodel = Admin(admin);
   //await adminmodel.save(); you can save data to database like this also
-
   await Admin.create(admin, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
   });
-  console.log(admin);
   res.send(admin);
 });
+
+
+router.put('/:id',async (req,res) => {
+  console.log(req.params.id);
+  try{
+    await Admin.findOneAndUpdate({_id: req.params.id},
+      { $set: 
+      {firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      mobile: req.body.mobile,
+      course: req.body.course}
+    },
+    { new: true },
+    (err,result)=>{
+      console.log("Result: ",result);
+    }
+    )
+  }
+  catch(err){
+    console.log(err);
+  };
+  res.json(await Admin.findById(req.params.id));
+})
+
+
+router.delete('/:id',async (req,res)=>{
+  try{
+    await Admin.findByIdAndDelete(req.params.id);
+  }
+  catch(err){
+    console.log("Err: ",err);
+  }
+  res.json(await Admin.find());
+})
+
 
 module.exports = router;
