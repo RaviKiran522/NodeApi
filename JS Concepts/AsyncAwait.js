c24c57e189afe0e40ad5baabfa424296b26f448a
RaviKiran522 <ravikirangundala9@gmail.com>
2024-02-21 12:13:08 +0530

    Async Awiat code changes

--- JS Concepts/AsyncAwait.js
+++ JS Concepts/AsyncAwait.js
@@ -4,9 +4,29 @@ const p1 = new Promise((resolve, reject) => {
     }, 2000)
 })
 
-async function getData() {
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("promise data2")
+    }, 5000)
+})
+
+async function getDataUsingAsyncAwait() {
     const data = await p1;
     console.log("data: ", data)
+    console.log("Async Await 1")
+
+    const data2 = await p2;
+    console.log("data2: ", data2);
+    console.log("Async Await 2")
 }
 
-getData()
+function getDataUsingPromises() {
+    p1.then((res) => console.log("res: ", res))
+    console.log("promises, called first")
+
+    p2.then((res) => console.log("res: ", res))
+    console.log("promises, called second")
+}
+
+getDataUsingAsyncAwait();
+getDataUsingPromises()
